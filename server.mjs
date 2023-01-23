3264991ccb87dae07425a7bde3bcd864b8f55040
mie035 <Notakes035@gmail.com>
2023-01-23 22:21:23 +0900

    mod

--- server.mjs
+++ server.mjs
@@ -1,6 +1,29 @@
-import { createServer } from 'http';
+import express from 'express'
+import fs from 'fs'
+const app = express()
+const port = process.env.PORT|| 3000;
 
-createServer((req, res) => {
-  res.write('Hello World!');
-  res.end();
-}).listen(process.env.PORT);
+app.get('/', (req, res) => {
+
+  res.send('Hello World!')
+})
+
+app.get('/get-one', (req, res) => {
+  let cnt = 0;
+  if(fs.existsSync("file1.txt")){
+    // 書き込み
+    cnt = Number(fs.readFileSync("file1.txt"));
+    console.log(`read file1.txt : ${cnt}`);
+  }
+    cnt += 1;
+    fs.writeFile("file1.txt", cnt.toString(), (err) => {
+      if (err) throw err;
+      console.log(`update file1.txt : ${cnt}`);
+    });
+  res.send(`cnt is ${cnt}`)
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
